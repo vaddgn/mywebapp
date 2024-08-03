f400c55c6a7baf00e461092b3113414603d82772
VadimD <144050549+vaddgn@users.noreply.github.com>
2024-08-03 15:03:16 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,9 +1,8 @@
 document.getElementById('surveyForm').addEventListener('submit', function(event) {
-    event.preventDefault();
     const name = document.getElementById('name').value;
 
     // Отправка данных в телеграм
     if (window.Telegram.WebApp) {
         window.Telegram.WebApp.sendData(JSON.stringify({ name }));
     }
-});
+}); 
