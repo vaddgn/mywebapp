0fa5024a895db8ace5aa48ac939f237becab5521
VadimD <144050549+vaddgn@users.noreply.github.com>
2024-08-03 21:39:35 +0300

    Update form.js

--- form.js
+++ form.js
@@ -9,6 +9,8 @@ tg.MainButton.onClick(function(){
     let data = {
         a : 'отправлено'
     };
+    let test = document.getElementById('test')
     tg.sendData(JSON.stringify(data));
     tg.MainButton.setText('Отправлено');
+    test.innerHTML = "Sending data: " . JSON.stringify(data)
 });
