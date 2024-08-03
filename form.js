4b06dee5f809734eb3f482bb94c3423ea1dcabac
VadimD <144050549+vaddgn@users.noreply.github.com>
2024-08-03 21:04:18 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,7 +1,13 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
 let btn = document.getElementById('btn');
-btn.addEventListener('click', function(){
+
+
+Telegram.WebApp.onEvent('mainButtonClicked', function (){
     let data = {
         a : 'отправлено'
     };
