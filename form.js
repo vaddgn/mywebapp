c38b05f4ea3cf229dcb96abfebeec0988652cea0
VadimD <144050549+vaddgn@users.noreply.github.com>
2024-08-03 20:46:17 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,32 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Survey Form</title>
-    <!-- Подключение скрипта Telegram Web Apps API -->
-    <script src="https://telegram.org/js/telegram-web-app.js"></script>
-</head>
-<body>
-    <h1>Survey Form</h1>
-    <form id="surveyForm">
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name"><br><br>
-        <input type="submit" value="Submit">
-    </form>
-    <script>
-        const tg = window.Telegram.WebApp;
-
-        document.getElementById('surveyForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const name = document.getElementById('name').value;
-
-            // Отправка данных в телеграм
-            tg.sendData(JSON.stringify({ name: name }));
-
-            // Закрытие WebApp после отправки данных
-            tg.close();
-        });
-    </script>
-</body>
-</html>
+let tg = window.Telegram.WebApp;
+tg.expand();
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+    let data = {
+        a : 'отправлено'
+    };
+    tg.sendData(JSON.stringify(data));
+});
